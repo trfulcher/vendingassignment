af13b2adaa5e700082af97232b1aa09223264fb3
trfulcher <36922004+trfulcher@users.noreply.github.com>
2022-03-03 20:56:07 +0000

    Update ISalesTransaction.java
    Tidy javadoc

--- src/main/java/org/trf/vend/model/ISalesTransaction.java
+++ src/main/java/org/trf/vend/model/ISalesTransaction.java
@@ -3,19 +3,24 @@ package org.trf.vend.model;
 public interface ISalesTransaction {
 
     /**
-     *
+     * Was used in early tests, should be deprecated from interface
      * @param b
      * @param oa
-     * @return
+     * @return TransactionResult encapsulates the success of the transaction, including possible change returned
      * @throws InsufficientFundsException
      */
     TransactionResult execute(Basket b, IOfferedAmount oa) throws InsufficientFundsException;
 
     /**
-     *
+     * Given the target sale price and a holder of all coins supplied (the IOfferedAmount) attempt to determine and return a result
+     * Possible outcomes -
+     * InsufficientFundsException thrown if offered amount less that basket amount
+     * result success and no change if exact amount supplied
+     * result success and a set of change in TransactionResult when IOfferedAmount greater than sale price
+     * result fail and all of IOfferedAmount in change if we can't offer suitable change
      * @param basketamount
      * @param oa
-     * @return
+     * @return TransactionResult encapsulates the success of the transaction, including possible change returned
      * @throws InsufficientFundsException
      */
     TransactionResult execute(int basketamount, IOfferedAmount oa) throws InsufficientFundsException;
